80a33ea834cde1a7cb1d62c275e00c81ad664494
Vulpelo <damian.praski@gmail.com>
2018-10-11 12:38:27 +0200

    Creating table of buttons

--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -10,8 +10,14 @@ using System.Windows.Forms;
 
 namespace Snake
 {
+
     public partial class Form1 : Form
     {
+        private int playRows = 10;
+        private int playColumns = 10;
+        TableLayoutPanel tLP;
+        Button[,] places;
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +31,38 @@ namespace Snake
         private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            tLP = new TableLayoutPanel();
+
+            tLP.ColumnCount = playColumns;
+            tLP.RowCount = playRows;
+
+            places = new Button[playColumns, playRows];
+
+            for (int i = 0; i < playColumns; i++)
+            {
+                for (int j = 0; j < playRows; j++)
+                {
+                    places[i, j] = new Button();
+                    places[i, j].Height = 10;
+                    places[i, j].Width = 10;
+                    var margs = places[i, j].Margin;
+                    margs.All = 0;
+                    places[i, j].Margin = margs;
+                    places[i, j].Enabled = false;
+
+                    tLP.Controls.Add(places[i,j]);
+                }
+            }
+            tLP.Width = 400;
+            tLP.Height = 400;
+            tLP.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            flowLayoutPanel1.Controls.Add(tLP);
+
+            PlayButton.Enabled = false;
         }
     }
 }
